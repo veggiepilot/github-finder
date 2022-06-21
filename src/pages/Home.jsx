044c521f713ac50c33442f24096ee22bce2768c8
veggiepilot <johanneschitura@gmail.com>
2022-06-20 22:03:03 -0400

    Added UserResults component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
-
+import UserResults from "../components/users/UserResults"
 
 function Home() {
   return (
-      <div>
-        <h1 className='text-6xl'>Welcome</h1>
-        {process.env.REACT_APP_GITHUB_TOKEN}
-      </div>
+      <>
+        <UserResults />
+      </>
     
   )
 }
