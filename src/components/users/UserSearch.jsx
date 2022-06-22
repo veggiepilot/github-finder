29977d89c365d4a2c409759e886378660adca940
veggiepilot <johanneschitura@gmail.com>
2022-06-22 19:59:10 -0400

    Moved getUser to actions file

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -6,7 +6,7 @@ import {searchUsers} from '../../context/github/GithubActions'
 function UserSearch() {
     const [text, setText] = useState('')
 
-    const {users, dispatch, clearUsers} = useContext(GithubContext)
+    const {users, dispatch} = useContext(GithubContext)
     const {setAlert} = useContext(AlertContext)
 
     const handleChange = (event) => setText(event.target.value)
@@ -47,7 +47,7 @@ function UserSearch() {
         </div>
         {users.length > 0 && (
             <div>
-                <button onClick={clearUsers} className='btn btn-ghost btn-lg'>
+                <button onClick={() => dispatch({ type: 'CLEAR_USERS'})} className='btn btn-ghost btn-lg'>
                     Clear
                 </button>
             </div>
